331364e7af6244f2280f04cb9a8c0b4ab42778c5
Ian McLoughlin <ian.mcloughlin@gmit.ie>
2017-01-30 13:51:25 +0000

    Cons reverse

--- quote.rkt
+++ quote.rkt
@@ -24,12 +24,52 @@
 '(+ 1 3)
 
 ; Create a list containing a, b and c.
-(define l '(a b c))
-l
+;(define m '(a b c))
+(define m (list 1 2 3))
+m
+
 
 ; Reverse it - note the use of append (similar to cons).
 (define (rev l)
   (if (null? l)
       null
       (append (rev (cdr l)) (list (car l)))))
-(rev l)
+
+(rev m)
+
+; Explaining the rev function, line by line.
+(null? m)
+(car m)
+(list (car m))
+(cdr m)
+(null? (cdr m))
+(list (car (cdr m)))
+(cdr (cdr m))
+(null? (cdr (cdr m)))
+(list (car (cdr (cdr m))))
+(cdr (cdr (cdr m)))
+(null? (cdr (cdr (cdr m))))
+(append (cdr (cdr m)) (list (car (cdr m))))
+; One line missing.
+
+
+; Reverse - with cons only.
+(cons 1 (list 2 3))
+
+(define (revwcons l)
+  (if (null? l)
+    null
+    (cons (car l) (cdr l))))
+
+(revwcons (list 1 2 3))
+
+(list (list (list 3) 2) 1)
+
+
+
+
+
+
+
+
+
