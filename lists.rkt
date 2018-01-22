1485bd5438a65e10cbbf352f02863714fde8223f
Ian McLoughlin <ian.mcloughlin@gmit.ie>
2018-01-22 10:47:25 +0000

    Clean up

--- lists.rkt
+++ lists.rkt
@@ -3,6 +3,12 @@
 ; The empty list.
 null
 
+; The Little Schemer, Commandment 2:
+;   Use cons to build lists.
+; The Little Schemer, The Law of Cons:
+;   The primitive cons takes two arguments.
+;   The second argument to cons muts be a list.
+;   The result is a list.
 ; Constructing a list from the empty list.
 (cons 5 null)
 (cons 4 (cons 5 null))
@@ -16,14 +22,22 @@ null
 ; Giving a list a name.
 (define l (list 1 2 3 4 5))
 
+; The Little Schemer, The Law of Car:
+;   The primitive car is defined only for non-empty lists.
 ; The first element of a list.
 (car l)
+
+; The Little Schemer, The Law of Cdr:
+;   The primitive cdr is defined only for non-empty lists.
+;   The cdr of any non-empty list is always another list.
 ; The rest of the list.
 (cdr l)
 ; The second element of the list.
 (car (cdr l))
 (cadr l)
 
+; The Little Schemer, The Law of Null?:
+;   The primitive null? is defined only for lists.
 ; Check if a list is empty.
 (null? l)
 
@@ -50,3 +64,17 @@ null
 (atom? 1)
 (atom? #t)
 (atom? null)
+
+; The Little Schemer, The Law of Eq?:
+;   The primitive eq? takes two arguments.
+;   Each must be a non-numeric atom.
+(define a "hello")
+(define b "hello")
+(define m 1)
+(define n 2)
+(eq? a b)
+(eq? m n)
+(eq? m m)
+(eq? 'x 'y)
+(eq? 'x 'x)
+(eq? 1 1)
